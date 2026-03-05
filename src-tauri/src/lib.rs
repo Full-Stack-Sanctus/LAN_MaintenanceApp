bb5fd8798cb2bb7832dc918196b7d721d19bb046
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2026-03-05 23:14:59 +0000

    Pushing commit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,7 @@ pub use commands::*;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default
+    tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![
             commands::execute_enterprise_audit
