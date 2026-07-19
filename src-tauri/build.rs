bc51f5bb1e241a8bd4d193ae4d2a0f6e70d7567f
Full-Stack-Sanctus <Full-Stack-Sanctus@gmail.com>
2026-07-19 14:11:42 +0000

    Initial commit for Version 2

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,12 +1,3 @@
 fn main() {
-    // Set up custom windows attributes
-    let mut windows = tauri_build::WindowsAttributes::new();
-    
-    // Inject your custom app.manifest file natively
-    windows = windows.app_manifest(include_str!("app.manifest"));
-
-    // Apply the attributes to Tauri's build system
-    let attrs = tauri_build::Attributes::new().windows_attributes(windows);
-    
-    tauri_build::try_build(attrs).expect("failed to run tauri build system");
+    tauri_build::build();
 }
